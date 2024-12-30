352e2a026957ab110456a32d1744d58b0c18a12c
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-12-30 00:04:42 +0000

    add matrix decomposition to TB x,y,z angles.

--- include/G4HalfSpaceTransformation.hh
+++ include/G4HalfSpaceTransformation.hh
@@ -7,9 +7,65 @@
 class G4HalfSpaceTransformation {
 public:
   G4HalfSpaceTransformation() = default;
+
   G4HalfSpaceTransformation(const G4ThreeVector &t,
-                            const G4ThreeVector &r) :
-                            _t(t), _r(r) {}
+                            const G4ThreeVector &r) : _t(t), _r(r) {
+  }
+
+  G4HalfSpaceTransformation(const G4ThreeVector &v) {
+
+    G4RotationMatrix rmatrix;
+
+    auto vnorm = v / v.mag();
+    G4ThreeVector zaxis = G4ThreeVector(0, 0, 1);
+    G4ThreeVector raxis = zaxis.cross(vnorm);
+    G4double angle = asin(raxis.mag());
+
+    if (angle == 0) {
+      raxis = G4ThreeVector(0, 0, 1);
+    }
+    rmatrix.set(raxis, angle);
+    rmatrix.rectify();
+
+    auto a_11 = rmatrix.xx();
+    auto a_12 = rmatrix.xy();
+    auto a_13 = rmatrix.xz();
+
+    auto a_21 = rmatrix.yx();
+    auto a_22 = rmatrix.yy();
+    auto a_23 = rmatrix.yz();
+
+    auto a_31 = rmatrix.zx();
+    auto a_32 = rmatrix.zy();
+    auto a_33 = rmatrix.zz();
+
+    if( fabs(a_31-1) < 1e-9 && a_31 > 1.0)
+      a_31 = 1.0;
+    else if( fabs(a_31+1) < 1e-9 && a_31 < -1.0)
+      a_31 = -1.0;
+
+    double x;
+    double y;
+    double z;
+
+    if( fabs(a_31) != 1) {
+      x = atan2(a_32, a_33);
+      y = asin(-a_31);
+      z = atan2(a_21, a_11);
+    }
+    else if(a_31 == -1) {
+      z = 0.0;
+      x = atan2(a_21, a_11) + z;
+      y = M_PI/2.0;
+    }
+    else if(a_31 == 1) {
+      z = 0.0;
+      x = atan2(-a_12, -a_13) - z;
+      y = -M_PI/2.0;
+    }
+
+    _r.set(x,y,z);
+  }
 
   G4HalfSpaceTransformation(const G4AffineTransform &a) {
     auto rmat = a.NetRotation();
