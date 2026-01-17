22f18ba5a7637cb63b76ca84debf7417f7836a0a
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-01-17 17:20:05 +0000

    WIP : FLUKA loading

--- include/G4LoaderFluka.hh
+++ include/G4LoaderFluka.hh
@@ -1,2 +1,30 @@
 #pragma once
 
+#include <string>
+
+class G4LoaderFluka {
+public:
+  G4LoaderFluka();
+  ~G4LoaderFluka();
+
+  void Load(std::string file_name);
+
+protected:
+  // states of loader
+  bool free = false;
+  int itransform = -1;
+  bool pp_include = true;
+
+  // preprocessor defines
+
+  // rototranslations
+
+  // bodies
+
+  // regions
+
+  // materials?
+
+  // material assignments
+
+};
---
+++ src/G4LoaderFluka.cc
@@ -0,0 +1,9 @@
+#include "G4LoaderFluka.hh"
+
+G4LoaderFluka::G4LoaderFluka() {}
+
+G4LoaderFluka::~G4LoaderFluka() {}
+
+void G4LoaderFluka::Load(std::string file_name) {
+
+}
